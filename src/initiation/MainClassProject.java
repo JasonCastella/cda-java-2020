e2bead9d76c4add497db2bdf8307119fcb211357
JAS.CASTELL <JAS.CASTELL@s124-12.pedago.mermoz.edu>
2020-09-29 10:15:09 +0200

    I ll be back

--- src/initiation/MainClassProject.java
+++ src/initiation/MainClassProject.java
@@ -3,8 +3,8 @@ package initiation;
 public class MainClassProject {
 
 	public static void main(String[] args) {
-System.out.println("Hello World !");
-
+System.out.println("Hello World, I'm Back");
+System.out.println("I'll be back soon !");
 	}
 
 }
