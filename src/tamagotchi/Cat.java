ecb1f7eaef7eb43cc3b60de1740f438ea9e74c80
JAS.CASTELL <JAS.CASTELL@s124-12.pedago.mermoz.edu>
2020-10-01 17:25:01 +0200

    Tamagotchi

---
+++ src/tamagotchi/Cat.java
@@ -0,0 +1,69 @@
+package tamagotchi;
+
+public class Cat {
+
+	private String name = "Inconnu";
+	private String color;
+	private int age = 0;
+
+	/**
+	 * Constructeur
+	 * 
+	 * @param name
+	 * @param color
+	 * @param age
+	 */
+	public Cat(String name, String color, int age) {
+		this.name = name;
+		this.color = color;
+		this.age = age;
+	}
+
+	public Cat(String color) {
+		this.color = color;
+		this.name= "Chat "+this.color;//Nom par defaut
+	}
+
+	/**
+	 * Définir un autre nom Mutateur (setter en Anglais)
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * Renvoie le nom du chat
+	 * @return
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void show() {
+		System.out.println("Chat nommé " + this.name + " de couleur " + this.color);
+		System.out.println(this.name + " a " + this.age + " ans");
+	}
+
+	public void listen() {
+		System.out.println("RRRRRRrrrrrrrr");
+	}
+	
+}
---
+++ src/tamagotchi/Tamagotchi.java
@@ -0,0 +1,139 @@
+package tamagotchi;
+
+public class Tamagotchi {
+
+	private String name;
+	private String specie;
+	private String color;
+	private String tall;
+	private int health;
+	private int lifeExpectancy;
+
+	public Tamagotchi(String name, String specie, String color, String tall, int health, int lifeExpectancy) {
+		this.name = name;
+		this.specie = specie;
+		this.color = color;
+		this.tall = tall;
+		this.health = health;
+		this.lifeExpectancy = lifeExpectancy;
+	}
+
+	public void chosenSpecie(String specie) {
+		if (specie.equals("Chien")) {
+			dogStats();
+		} else if (specie.equals("Chat")) {
+			catStats();
+		} else if (specie.equals("Ecureuil")) {
+			squirrelStats();
+		} else if (specie.equals("Lapin")) {
+			rabbitStats();
+		} else
+			dragonStats();
+	}
+
+	public void dogStats() {
+		this.tall = "70cm";
+		this.health = 5;
+		this.lifeExpectancy = 15;
+	}
+
+	public void catStats() {
+		this.tall = "20cm";
+		this.health = 5;
+		this.lifeExpectancy = 15;
+	}
+
+	public void squirrelStats() {
+		this.tall = "15cm";
+		this.health = 4;
+		this.lifeExpectancy = 10;
+	}
+
+	public void rabbitStats() {
+		this.tall = "20cm";
+		this.health = 4;
+		this.lifeExpectancy = 10;
+	}
+
+	public void dragonStats() {
+		this.tall = "3m";
+		this.health = 10;
+		this.lifeExpectancy = 30;
+	}
+
+	public void show() {
+		System.out.println("C'est un " + specie + " qui a pour nom " + name + ",");
+		System.out.println("Il est " + color + " et mesure environ " + tall + ",");
+		System.out.println(
+				"Il a " + health + " point de vie, et son espérance de vie est de " + lifeExpectancy + " jours.");
+	}
+
+	public void feed() {
+		System.out.println("Vous venez de nourrir " + this.name + ", il est rassasié !");
+	}
+
+	public void hungry() {
+
+	}
+
+	public void toilet() {
+		System.out.println(this.name + " se sent mieux, il avait besoin de faire ses besoins !");
+	}
+
+	public void toiletNeeds() {
+
+	}
+
+	public void sleep() {
+		System.out.println(this.name + " est extenué, il est temps pour lui de faire dodo");
+	}
+
+	public void tired() {
+
+	}
+
+	public void wash() {
+		System.out.println(this.name + " fait sa toilette, c'est agréable d'être propre quand même... ");
+	}
+
+	public void dirty() {
+
+	}
+
+	public void friends() {
+		
+	}
+
+	public void sex() {
+
+	}
+
+	public void socialNeeds() {
+
+	}
+
+	public void physicalActivity() {
+
+	}
+
+	public void hairDye() {
+
+	}
+
+	public void mood() {
+
+	}
+
+	public void death() {
+
+	}
+
+	public void showHealth() {
+		System.out.println("Il reste " + health + " point de vie a votre Tamagotchi.");
+	}
+
+	public void showActions() {
+		System.out.println("Manger\nDormir\nSe Laver\nFaire ses besoins\nFaire de l'exercice\nAller voir des amis\nSe reproduire\nExprimer son humeur\nEnlever ou mettre un masque\nSe teindre les cheveux");
+	}
+	
+}
